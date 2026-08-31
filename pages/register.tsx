8d052139f7d9fe0ee9f91fd950e90d9281f7e80e
Origami-Tobiichi <cyberdark454@gmail.com>
2026-08-31 18:23:29 +0800

    Add useEffect import to register page

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,8 +1,8 @@
 import Register from '../components/Register';
 import Navbar from '../components/Navbar';
 import { useAuth } from '../context/AuthContext';
-import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function RegisterPage() {
   const { user } = useAuth();
